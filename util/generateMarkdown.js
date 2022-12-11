3089ba82ada051620af61b29cdbaa025039bfcb6
Raxopagne <OskarZaorski1@gmail.com>
2022-12-11 19:34:03 +0000

    Finished off generate markdown added license links

--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -1,18 +1,88 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  }
+  else if (license === 'GPL 3.0') {
+    return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+  }
+  else if (license === 'APACHE 2.0'){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  }
+  else if (license === 'BSD 3'){
+    return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+  }
+  else if (license === 'MPL 2.0'){
+    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`;
+  }
+   return '';
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  return '';
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license, licenseChoice) {
+  if(license == "None"){
+    return "";
+   }
+
+   var badge =renderLicenseBadge(license, licenseChoice);
+   var link =renderLicenseLink(license,renderLicenseLink);
+
+   return badge + " \r" + link + " ";
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(data, licenseChoice) {
+  var sec = renderLicenseSection(data.license, licenseChoice)
+  return `
+  # 
+  ${data.title} ${renderLicenseBadge(data.License)}
+  ${sec}
+  # Contents table
+  *[Description](#Description)
+  *[Installation}(#Install)
+  *[Instruction](#Instructions)
+  *[Contributors](#Contributors)
+  *[Test](#Test)
+  *${renderLicenseLink}
+  *[Questions](#Questions)
+
+  ## Description
+  ${Data.Description}
+
+  ## Installation
+  You install this project by ${data.Install}
+
+  ## Instructions
+  You use this project by ${data.Instructions}
+
+  ## Contributors
+   Please list the Contributors ${data.Contributors}
+
+  ## Test
+  This is how you run a test ${data.Test}
+
+  ## License
+  This project uses the following licenses ${data.License}
+
+  ## Questions 
+  Any questions contact me here: 
+
+  Github:[${data.GitHub}](https://github.com/${data.GitHub})
+
+  Email:[${data.Email}](mailto:${data.Email})
 
 `;
 }
--- util/index.js
+++ util/index.js
@@ -2,73 +2,70 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const util = require("util");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkdown = require("./generateMarkdown");
 
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        message: "What is your project title?",
-        name: "Title"
-    },
-    {
-        type: "input",
-        message: "Describe your project",
-        name: "Description"
-    },
-    {
-        type: "input",
-        message: "What are the steps required to install your project?",
-        name: "Installation"
-    },
-    {
-        type: "input",
-        message: "Please provide instructions for use",
-        name: "Usage"
-    },
-    {
-        type: "input",
-        message: "Are there any contributors?",
-        name: "Contributors"
-    },
-    {
-        type: "input",
-        message: "How do you test your project?",
-        name: "Test"
-    },
-    {
-        type: "list",
-        message: "Please select a title",
-        name: "License",
-        choices: [
-            "MIT",
-            "GVL-GPL 3.0",
-            "APACHE 2.0",
-            "BSD 3",
-            "None"
-        ]
-    },
-    {
-        type: "input",
-        message: "Contact info for further questions",
-        name: "Questions"
-    },
-    {
-        type: "input",
-        message: "Please enter your Github username",
-        name: "Username"
-    },
-    {
-        type: "input",
-        message: "What is your email?",
-        name: "Email"
-    },
-];
+function init() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            message: "What is your project title?",
+            name: "Title"
+        },
+        {
+            type: "input",
+            message: "Describe your project",
+            name: "Description"
+        },
+        {
+            type: "input",
+            message: "What are the steps required to install your project?",
+            name: "Installation"
+        },
+        {
+            type: "input",
+            message: "Please provide instructions for use",
+            name: "Instructions"
+        },
+        {
+            type: "input",
+            message: "Are there any contributors?",
+            name: "Contributors"
+        },
+        {
+            type: "input",
+            message: "How do you test your project?",
+            name: "Test"
+        },
+        {
+            type: "list",
+            message: "Please select a title",
+            name: "License",
+            choices: [
+                "MIT",
+                "GVL-GPL 3.0",
+                "APACHE 2.0",
+                "BSD 3",
+                "MPL 2.0",
+                "None"
+            ]
+        },
+        {
+            type: "input",
+            message: "Contact info for further questions",
+            name: "Questions"
+        },
+    ])
+        .then(answers => {
+            const markdown=generateMarkdown(answers)
+            writeToFile(markdown)
+        });
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, err => {
+    fs.writeFileSync(fileName, data, err => {
         if (err) {
             return console.log(err)
         } else {
